38546d99e1c9b608f3453a1cc1ac5f2f712bde6c
mattmc3 <mattmc3@gmail.com>
2024-03-06 10:31:02 -0500

    Bugfix
    - Ensure extended_glob
    - Don't be agro about setting XDG vars
    - null globbing for missing dirs

--- zfunctions.plugin.zsh
+++ zfunctions.plugin.zsh
@@ -3,12 +3,14 @@
 # Set $0.
 0=${(%):-%N}
 
+setopt extended_glob
+
 # Use $ZFUNCDIR as functions directory.
-: ${ZFUNCDIR:=${ZDOTDIR:-${XDG_CONFIG_HOME:=$HOME/.config}/zsh}/functions}
+: ${ZFUNCDIR:=${ZDOTDIR:-${XDG_CONFIG_HOME:-$HOME/.config}/zsh}/functions}
 [[ -d $ZFUNCDIR ]] || mkdir -p $ZFUNCDIR
 
 # Autoload Zsh function dir and its subdirs.
-for _zfuncdir in ${0:a:h}/functions $ZFUNCDIR $ZFUNCDIR/*(N/); do
+for _zfuncdir in ${0:a:h}/functions $ZFUNCDIR(N) $ZFUNCDIR/*(N/); do
   fpath=($_zfuncdir $fpath)
   autoload -Uz $_zfuncdir/*(.:t)
 done
